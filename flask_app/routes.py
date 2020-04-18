9df5a8b0913098ffd1ad792b8348b14792e72a27
AndrewSLowe <andrewstevenlowe@gmail.com>
2020-04-18 18:13:26 -0400

    fda

--- flask_app/routes.py
+++ flask_app/routes.py
@@ -11,11 +11,12 @@ recommend_time = Blueprint('recommend_time', __name__)
 def recommendation():
     user_input = request.get_json()
     _id = user_input['id']
+    twitter_handle = user_input['screenname']
 
     backend_url = 'https://social-media-strategy.herokuapp.com/api/posts/' + str(_id)
     header_data = {'Authorization' : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWJqZWN0IjoxLCJlbWFpbCI6ImRzMTBAbGFzZXJzaGFya3MuY29tIiwib2t0YV91c2VyaWQiOiJEUyBoYXZlIG5vIE9rdGEiLCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE1ODcyNDYyMTgsImV4cCI6MTU4OTgzODIxOH0.ywj1wNvEFh2sSOv0Oc_h1MFXohuHAN1OJQtpCJVKMsk"}
 
-    dw = data_wrangling('ChrisAlbon', 5)
+    dw = data_wrangling(twitter_handle, 5)
     fi = dw.followers_ids()
     get_follower_data = dw.get_follower_data(fi)
     optimal_time = dw.optimal_time(get_follower_data)
